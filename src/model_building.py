74589aa354c2d124af7b0469f1e3775498cf4ce2
Sinchana Venugopal <sinchanavenugopal@gmail.com>
2025-01-30 19:38:41 +0530

    Introduced params.yaml

--- src/model_building.py
+++ src/model_building.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
+import yaml
 
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
+
+n_estimators = yaml.safe_load(open("params.yaml", "r"))["model_building"]["n_estimators"]
 
 train_data = pd.read_csv("./data/processed/train_processed_data.csv")
 
@@ -15,7 +18,7 @@ train_data = pd.read_csv("./data/processed/train_processed_data.csv")
 X_train = train_data.drop(columns= ['Potability'], axis=1)
 y_train = train_data['Potability']
 
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators= n_estimators)
 clf.fit(X_train, y_train)
 
 pickle.dump(clf, open("model.pkl", "wb"))
